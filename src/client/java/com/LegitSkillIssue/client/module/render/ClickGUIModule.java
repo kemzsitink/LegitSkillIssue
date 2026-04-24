1d7298feac8a433da4f08ae5a237b3149c264730
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 12:33:01 -0700

    Refactor: Switch GUI from MCEF/HTML to Elementa UI for better performance and integration

--- src/client/java/com/LegitSkillIssue/client/module/render/ClickGUIModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/render/ClickGUIModule.java
@@ -2,9 +2,18 @@ package com.LegitSkillIssue.client.module.render;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import com.LegitSkillIssue.client.gui.ClickGuiScreen;
 
 public class ClickGUIModule extends Module {
     public ClickGUIModule() {
-        super("ClickGUI", "ClickGUI module for RENDER", Category.RENDER);
+        super("ClickGUI", "Opens the module menu.", Category.RENDER);
+    }
+
+    @Override
+    protected void onEnable() {
+        if (mc.currentScreen == null) {
+            mc.setScreen(new ClickGuiScreen());
+        }
+        setEnabled(false); // Toggle off immediately so it can be re-enabled to open again
     }
 }
