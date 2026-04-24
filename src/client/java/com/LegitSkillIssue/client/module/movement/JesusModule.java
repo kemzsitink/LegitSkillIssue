8787aad68350a950b9977a5fd69505b22e33f534
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:07:38 -0700

    feat(movement): hoàn thành logic cho module Jesus

--- src/client/java/com/LegitSkillIssue/client/module/movement/JesusModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/JesusModule.java
@@ -2,9 +2,20 @@ package com.LegitSkillIssue.client.module.movement;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import net.minecraft.util.math.Vec3d;
 
 public class JesusModule extends Module {
     public JesusModule() {
-        super("Jesus", "Jesus module for MOVEMENT", Category.MOVEMENT);
+        super("Jesus", "Allows you to walk on water.", Category.MOVEMENT);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+
+        if (mc.player.isTouchingWater() || mc.player.isInLava()) {
+            Vec3d vel = mc.player.getVelocity();
+            mc.player.setVelocity(vel.x, 0.11, vel.z);
+        }
     }
 }
