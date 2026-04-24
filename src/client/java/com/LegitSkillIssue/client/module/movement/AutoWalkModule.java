b75420c92c6999390aaacb2fb0d949240b814482
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:16:32 -0700

    feat(movement): hoàn thành logic cho module AutoWalk

--- src/client/java/com/LegitSkillIssue/client/module/movement/AutoWalkModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/AutoWalkModule.java
@@ -5,6 +5,20 @@ import com.LegitSkillIssue.client.module.Category;
 
 public class AutoWalkModule extends Module {
     public AutoWalkModule() {
-        super("AutoWalk", "AutoWalk module for MOVEMENT", Category.MOVEMENT);
+        super("AutoWalk", "Automatically walks forward.", Category.MOVEMENT);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player != null) {
+            mc.options.forwardKey.setPressed(true);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (mc.options != null) {
+            mc.options.forwardKey.setPressed(false);
+        }
     }
 }
