134aa35e078f13dd5e951f89d3f15ca43212c98a
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:18:50 -0700

    feat(movement): hoàn thành logic cho module IceSpeed

--- src/client/java/com/LegitSkillIssue/client/module/movement/IceSpeedModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/IceSpeedModule.java
@@ -2,9 +2,28 @@ package com.LegitSkillIssue.client.module.movement;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import com.LegitSkillIssue.client.setting.NumberSetting;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.Vec3d;
 
 public class IceSpeedModule extends Module {
+    public final NumberSetting speed = new NumberSetting("Speed", 1.5, 1.0, 5.0, 0.1);
+
     public IceSpeedModule() {
-        super("IceSpeed", "IceSpeed module for MOVEMENT", Category.MOVEMENT);
+        super("IceSpeed", "Makes you move faster on ice.", Category.MOVEMENT);
+        addSetting(speed);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null || mc.world == null) return;
+
+        if (mc.world.getBlockState(mc.player.getBlockPos().down()).getBlock() == Blocks.ICE ||
+            mc.world.getBlockState(mc.player.getBlockPos().down()).getBlock() == Blocks.PACKED_ICE ||
+            mc.world.getBlockState(mc.player.getBlockPos().down()).getBlock() == Blocks.BLUE_ICE) {
+            
+            Vec3d vel = mc.player.getVelocity();
+            mc.player.setVelocity(vel.x * speed.getValue(), vel.y, vel.z * speed.getValue());
+        }
     }
 }
