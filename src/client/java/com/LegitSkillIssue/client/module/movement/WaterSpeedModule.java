e80dc466d5f9975106b12d5333b7863eabfc585a
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:19:23 -0700

    feat(movement): hoàn thành logic cho module WaterSpeed

--- src/client/java/com/LegitSkillIssue/client/module/movement/WaterSpeedModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/WaterSpeedModule.java
@@ -2,9 +2,24 @@ package com.LegitSkillIssue.client.module.movement;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import com.LegitSkillIssue.client.setting.NumberSetting;
+import net.minecraft.util.math.Vec3d;
 
 public class WaterSpeedModule extends Module {
+    public final NumberSetting speed = new NumberSetting("Speed", 1.5, 1.0, 5.0, 0.1);
+
     public WaterSpeedModule() {
-        super("WaterSpeed", "WaterSpeed module for MOVEMENT", Category.MOVEMENT);
+        super("WaterSpeed", "Makes you swim faster.", Category.MOVEMENT);
+        addSetting(speed);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+
+        if (mc.player.isTouchingWater()) {
+            Vec3d vel = mc.player.getVelocity();
+            mc.player.setVelocity(vel.x * speed.getValue(), vel.y, vel.z * speed.getValue());
+        }
     }
 }
