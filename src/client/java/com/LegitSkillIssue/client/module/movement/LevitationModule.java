0ac50c1a536a2e09d7c7dd887d9be4e0f1b05f5e
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:22:17 -0700

    feat(movement): hoàn thành logic cho module Levitation

--- src/client/java/com/LegitSkillIssue/client/module/movement/LevitationModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/LevitationModule.java
@@ -2,9 +2,22 @@ package com.LegitSkillIssue.client.module.movement;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import com.LegitSkillIssue.client.setting.NumberSetting;
+import net.minecraft.util.math.Vec3d;
 
 public class LevitationModule extends Module {
+    public final NumberSetting speed = new NumberSetting("Speed", 0.1, 0.0, 1.0, 0.01);
+
     public LevitationModule() {
-        super("Levitation", "Levitation module for MOVEMENT", Category.MOVEMENT);
+        super("Levitation", "Makes you float upwards.", Category.MOVEMENT);
+        addSetting(speed);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+
+        Vec3d vel = mc.player.getVelocity();
+        mc.player.setVelocity(vel.x, speed.getValue(), vel.z);
     }
 }
