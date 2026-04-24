9cc0e2b1b7401450bafa08d9e86a239aa5dc53df
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:06:36 -0700

    feat(movement): hoàn thành logic cho module HighJump

--- src/client/java/com/LegitSkillIssue/client/module/movement/HighJumpModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/HighJumpModule.java
@@ -2,9 +2,24 @@ package com.LegitSkillIssue.client.module.movement;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import com.LegitSkillIssue.client.setting.NumberSetting;
+import net.minecraft.util.math.Vec3d;
 
 public class HighJumpModule extends Module {
+    public final NumberSetting height = new NumberSetting("Height", 1.5, 1.0, 5.0, 0.1);
+
     public HighJumpModule() {
-        super("HighJump", "HighJump module for MOVEMENT", Category.MOVEMENT);
+        super("HighJump", "Increases your jump height.", Category.MOVEMENT);
+        addSetting(height);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+
+        if (mc.options.jumpKey.isPressed() && mc.player.isOnGround()) {
+            Vec3d vel = mc.player.getVelocity();
+            mc.player.setVelocity(vel.x, 0.42 * height.getValue(), vel.z);
+        }
     }
 }
