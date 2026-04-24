e57c496ac6e37d3f6ede1433e2f9927e16215f1e
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 12:54:04 -0700

    feat(combat): hoàn thành logic cho module HitBox (sử dụng Mixin)

---
+++ src/client/java/com/LegitSkillIssue/client/mixin/EntityMixin.java
@@ -0,0 +1,25 @@
+package com.LegitSkillIssue.client.mixin;
+
+import com.LegitSkillIssue.client.module.ModuleManager;
+import com.LegitSkillIssue.client.module.combat.HitBoxModule;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Box;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Inject(method = "getTargetingBoundingBox", at = @At("RETURN"), cancellable = true)
+    private void onGetTargetingBoundingBox(CallbackInfoReturnable<Box> cir) {
+        HitBoxModule hitBox = (HitBoxModule) ModuleManager.INSTANCE.getModules().stream()
+                .filter(m -> m instanceof HitBoxModule)
+                .findFirst().orElse(null);
+
+        if (hitBox != null && hitBox.isEnabled()) {
+            double expand = hitBox.expand.getValue();
+            cir.setReturnValue(cir.getReturnValue().expand(expand));
+        }
+    }
+}
--- src/client/java/com/LegitSkillIssue/client/module/combat/HitBoxModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/combat/HitBoxModule.java
@@ -2,9 +2,13 @@ package com.LegitSkillIssue.client.module.combat;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import com.LegitSkillIssue.client.setting.NumberSetting;
 
 public class HitBoxModule extends Module {
+    public final NumberSetting expand = new NumberSetting("Expand", 0.1, 0.0, 1.0, 0.05);
+
     public HitBoxModule() {
-        super("HitBox", "HitBox module for COMBAT", Category.COMBAT);
+        super("HitBox", "Expands entity hitboxes.", Category.COMBAT);
+        addSetting(expand);
     }
 }
