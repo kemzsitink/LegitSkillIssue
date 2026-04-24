221f8290339da6b727d07f5f52f43fe6f2531954
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 14:02:19 -0700

    feat(world): hoàn thành logic cho module StashFinder (Basic)

--- src/client/java/com/LegitSkillIssue/client/module/world/StashFinderModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/world/StashFinderModule.java
@@ -2,9 +2,37 @@ package com.LegitSkillIssue.client.module.world;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.util.math.BlockPos;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StashFinderModule extends Module {
+    private final List<BlockPos> stashes = new ArrayList<>();
+
     public StashFinderModule() {
-        super("StashFinder", "StashFinder module for WORLD", Category.WORLD);
+        super("StashFinder", "Finds large groups of containers.", Category.WORLD);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null || mc.world == null) return;
+        stashes.clear();
+
+        BlockPos playerPos = mc.player.getBlockPos();
+        int r = 64;
+
+        for (int x = -r; x <= r; x += 4) {
+            for (int y = -r; y <= r; y += 4) {
+                for (int z = -r; z <= r; z += 4) {
+                    BlockPos pos = playerPos.add(x, y, z);
+                    var block = mc.world.getBlockState(pos).getBlock();
+                    if (block == Blocks.CHEST || block instanceof ShulkerBoxBlock) {
+                        stashes.add(pos);
+                    }
+                }
+            }
+        }
     }
 }
