680e20a6a8dd41b0aafb3e39779c0ef6ab6f3fd4
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:09:06 -0700

    feat(movement): hoàn thành logic cho module Spider

--- src/client/java/com/LegitSkillIssue/client/module/movement/SpiderModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/SpiderModule.java
@@ -2,9 +2,20 @@ package com.LegitSkillIssue.client.module.movement;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import net.minecraft.util.math.Vec3d;
 
 public class SpiderModule extends Module {
     public SpiderModule() {
-        super("Spider", "Spider module for MOVEMENT", Category.MOVEMENT);
+        super("Spider", "Allows you to climb walls.", Category.MOVEMENT);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+
+        if (mc.player.horizontalCollision) {
+            Vec3d vel = mc.player.getVelocity();
+            mc.player.setVelocity(vel.x, 0.2, vel.z);
+        }
     }
 }
