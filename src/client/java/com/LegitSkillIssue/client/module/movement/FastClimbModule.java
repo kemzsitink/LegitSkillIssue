e4c1a6dc308835a2d7988c6ccb98950b240fcb32
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:20:59 -0700

    feat(movement): hoàn thành logic cho module FastClimb

--- src/client/java/com/LegitSkillIssue/client/module/movement/FastClimbModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/FastClimbModule.java
@@ -2,9 +2,24 @@ package com.LegitSkillIssue.client.module.movement;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import com.LegitSkillIssue.client.setting.NumberSetting;
+import net.minecraft.util.math.Vec3d;
 
 public class FastClimbModule extends Module {
+    public final NumberSetting speed = new NumberSetting("Speed", 1.5, 1.0, 5.0, 0.1);
+
     public FastClimbModule() {
-        super("FastClimb", "FastClimb module for MOVEMENT", Category.MOVEMENT);
+        super("FastClimb", "Makes you climb ladders faster.", Category.MOVEMENT);
+        addSetting(speed);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+
+        if (mc.player.isClimbing() && mc.player.horizontalCollision) {
+            Vec3d vel = mc.player.getVelocity();
+            mc.player.setVelocity(vel.x, speed.getValue() * 0.2, vel.z);
+        }
     }
 }
