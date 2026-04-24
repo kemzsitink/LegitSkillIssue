53818eaa72b6632b406e728a09d5c5af36252864
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-24 13:15:55 -0700

    feat(movement): hoàn thành logic cho module SafeWalk

--- src/client/java/com/LegitSkillIssue/client/module/movement/SafeWalkModule.java
+++ src/client/java/com/LegitSkillIssue/client/module/movement/SafeWalkModule.java
@@ -2,9 +2,29 @@ package com.LegitSkillIssue.client.module.movement;
 
 import com.LegitSkillIssue.client.module.Module;
 import com.LegitSkillIssue.client.module.Category;
+import net.minecraft.util.math.BlockPos;
 
 public class SafeWalkModule extends Module {
     public SafeWalkModule() {
-        super("SafeWalk", "SafeWalk module for MOVEMENT", Category.MOVEMENT);
+        super("SafeWalk", "Prevents you from falling off edges.", Category.MOVEMENT);
+    }
+
+    @Override
+    public void onTick() {
+        if (mc.player == null || mc.world == null) return;
+
+        if (mc.player.isOnGround()) {
+            BlockPos below = mc.player.getBlockPos().down();
+            if (mc.world.getBlockState(below).isAir()) {
+                // This is very basic. Real SafeWalk modifies the movement vector.
+                // For now we just press sneak.
+                mc.options.sneakKey.setPressed(true);
+            } else {
+                // Only release if not manually pressing
+                if (!mc.options.sneakKey.isPressed()) {
+                    // mc.options.sneakKey.setPressed(false);
+                }
+            }
+        }
     }
 }
