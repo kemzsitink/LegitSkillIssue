f7925bba04f5dc1fe0fa68b5eb21a196702b237e
kemzsitink <hocctiengchokeu@gmail.com>
2026-04-25 00:26:24 -0700

    feat(gui): thêm phím tắt Right Shift để mở ClickGUI

--- src/client/java/com/LegitSkillIssue/client/LegitSkillIssueClient.java
+++ src/client/java/com/LegitSkillIssue/client/LegitSkillIssueClient.java
@@ -1,12 +1,35 @@
 package com.LegitSkillIssue.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import com.LegitSkillIssue.client.module.ModuleManager;
+import com.LegitSkillIssue.client.gui.ClickGuiScreen;
+import org.lwjgl.glfw.GLFW;
 
 public class LegitSkillIssueClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		ModuleManager.INSTANCE.init();
-	}
+    private static KeyBinding guiKey;
+
+    @Override
+    public void onInitializeClient() {
+        // Register Module Manager
+        ModuleManager.INSTANCE.init();
+
+        // Register GUI Key (Right Shift)
+        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.legitskillissue.gui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                "category.legitskillissue.main"
+        ));
+
+        // Listen for key press
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (guiKey.wasPressed()) {
+                client.setScreen(new ClickGuiScreen());
+            }
+        });
+    }
 }
